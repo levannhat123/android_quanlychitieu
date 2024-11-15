6fa53da60c3c173b0c845c1ecbd4cef22f3e5ec7
Lê Văn Nhật <levannhat.23082003@gmail.com>
2024-11-15 08:24:20 +0700

    Menu Drawer

--- app/src/main/java/com/example/android_quanlychitieu/tablayout_khoanchi.java
+++ app/src/main/java/com/example/android_quanlychitieu/tablayout_khoanchi.java
@@ -1,20 +1,31 @@
 package com.example.android_quanlychitieu;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 public class tablayout_khoanchi extends AppCompatActivity {
     private TabLayout mtabLayout;
     private ViewPager mviewPage;
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tablayout_khoanchi);
-
+        drawerLayout = findViewById(R.id.drawer_layout);
         mtabLayout = findViewById(R.id.tab_layout);
         mviewPage = findViewById(R.id.viewpager);
 
@@ -24,5 +35,42 @@ public class tablayout_khoanchi extends AppCompatActivity {
 
         // Kết nối TabLayout với ViewPager
         mtabLayout.setupWithViewPager(mviewPage);
+
+        ImageView menuIcon = findViewById(R.id.menu_icon);
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_khoanthu) {
+                    // Xử lý cho khoản thu
+                } else if (id == R.id.nav_khoanchi) {
+                    // Xử lý cho khoản chi
+                } else if (id == R.id.nav_thongke) {
+                    // Xử lý cho thống kê
+                }  else if (id == R.id.nav_thoat) {
+                    // Xử lý cho thoát
+                    finish();
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+
     }
+
 }
